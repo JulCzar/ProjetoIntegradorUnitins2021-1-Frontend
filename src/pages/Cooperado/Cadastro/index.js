e959a3e3e78307b5ad827d240b9f7dc6d1697899
JulCzar <julczar21@outlook.com>
2021-04-19 13:44:12 -0300

    Created Select Menu component and adde mask to Input

--- src/pages/Cooperado/Cadastro/index.js
+++ src/pages/Cooperado/Cadastro/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import { CardHeader, UnInput } from '~/common/components'
+import { CardHeader, UnInput, UnSelect } from '~/common/components'
 import { Card, Container, Content, InputWrapper, UnForm } from '~/common/styles'
 import { Button } from '~/primereact'
 
 const Cadastro = () => {
+	const [groupOptions] = React.useState([
+		{label: 'Recanto', value: 1},
+		{label: 'Cargueiros', value: 2},
+		{label: 'Brejão', value: 3},
+		{label: 'Veredas', value: 4},
+		{label: 'Itabinhas', value: 5}
+	])
+
 	function cadastrar(form) {
 		// eslint-disable-next-line no-console
 		console.log('cadastro não implementado', form)
@@ -21,12 +29,14 @@ const Cadastro = () => {
 						</InputWrapper>
 						<UnInput name='email' label='Email'/>
 						<InputWrapper columns={2} gap='10px'>
-							<UnInput name='cpf' label='CPF' required/>
-							<UnInput name='phone' label='Telefone' required/>
-							<UnInput name='register' label='# do Registro' required/>
-							<UnInput name='group' label='Grupo de Usuário' required/>
+							<UnInput name='cpf' mask='999.999.999-99' label='CPF' required/>
+							<UnInput name='phone' mask='(99) 9 9999-9999' label='Telefone' required/>
 						</InputWrapper>
-						<UnInput type='password' name='password' label='Senha' requires/>
+						<InputWrapper columns={2} gap='10px'>
+							<UnInput name='register' label='# do Registro' required/>
+							<UnSelect name='group' label='Grupo de Usuário' options={groupOptions} required/>
+						</InputWrapper>
+						<UnInput type='password' name='password' label='Senha' required/>
 						<UnInput type='password' name='passwordConfirm' label='Confirmação de Senha' required/>
 						<Button type='submit' label='Cadastrar'/>
 					</UnForm>
