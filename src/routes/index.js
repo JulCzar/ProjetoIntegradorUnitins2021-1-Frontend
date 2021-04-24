0b5c2a8212347a0e75ba71856c5976dfc983d093
JulCzar <julczar21@outlook.com>
2021-04-24 14:35:15 -0300

    start of reports

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom'
 
 import { HelloWorld, Login, PageNotFound } from '~/pages'
 import Cadastro from '~/pages/Cooperado/Cadastro'
+import Relatorio from '~/pages/Cooperado/Relatorio'
 
 const Routes = function _Routes() {
 	return (
@@ -11,6 +12,7 @@ const Routes = function _Routes() {
 				<Route path='/' component={HelloWorld} exact/>
 				<Route path='/login' component={Login}/>
 				<Route path='/cadastrar' component={Cadastro}/>
+				<Route path='/relatorio' component={Relatorio}/>
 				<Route path='*' component={PageNotFound}/>
 			</Switch>
 		</BrowserRouter>
